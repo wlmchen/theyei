1b907d0df0df324a1c1ab4cc72c1038d18654327
MasonWang025 <mason.wang0025@gmail.com>
2020-12-29 13:07:46 -0800

    spacing update:

--- components/global/Header/NavItem.tsx
+++ components/global/Header/NavItem.tsx
@@ -11,7 +11,7 @@ export default function NavItem({
   const activeStyles = "border-yei-primary-main text-gray-900";
   const defaultStyles =
     "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700";
-  const [navItemStyle, setNavItemStyle] = useState(
+  const [navItemStyle, _] = useState(
     `inline-flex items-center px-1 pt-1 border-b-2 text-lg font-medium ${
       active ? activeStyles : defaultStyles
     }`
--- components/global/Header/Navbar.tsx
+++ components/global/Header/Navbar.tsx
@@ -4,6 +4,9 @@ import { useRouter } from "next/router";
 import routes from "./../../../data/routes";
 import NavItem from "./NavItem";
 
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Navbar() {
   const currPath = useRouter().pathname;
 
@@ -12,7 +15,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
-            <div className="-ml-2 mr-2 flex items-center md:hidden">
+            <div className="-ml-2 mr-2 flex items-center lg:hidden">
               {/* Mobile menu button */}
               <button
                 className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-yei-primary-main"
@@ -65,39 +68,25 @@ export default function Navbar() {
             </div>
             <div className="flex-shrink-0 flex items-center">
               <img
-                className="block lg:hidden h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark-yei-primary-main.svg"
-                alt="Workflow"
-              />
-              <img
-                className="hidden lg:block h-8 w-auto"
+                className="block h-9 w-auto"
                 src="/img/logos/yei-logo-full.png"
                 alt="YEI logo"
               />
             </div>
-            <div className="hidden md:ml-6 md:flex md:space-x-8">
-              {routes.map((r) => {
-                if (r.dropRoutes) {
-                  return (
-                    <NavItem
-                      key={r.name}
-                      name={r.name}
-                      href={r.href}
-                      active={r.href == currPath}
-                      dropRoutes={r.dropRoutes}
-                    />
-                  );
-                }
-
-                return (
-                  <NavItem
-                    key={r.name}
-                    name={r.name}
-                    href={r.href}
-                    active={r.href == currPath}
-                  />
-                );
-              })}
+            <div className="hidden lg:ml-6 lg:flex lg:space-x-6 xl:space-x-8">
+              {routes.map((r) => (
+                <NavItem
+                  key={r.name}
+                  name={r.name}
+                  href={r.href}
+                  dropRoutes={r.dropRoutes}
+                  active={
+                    r.dropRoutes
+                      ? currPath.startsWith(r.href)
+                      : r.href == currPath
+                  }
+                />
+              ))}
             </div>
           </div>
           <div className="flex items-center">
@@ -107,20 +96,8 @@ export default function Navbar() {
                 className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-yei-primary-main shadow-sm hover:bg-yei-primary-darker focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yei-primary-main"
               >
                 {/* Heroicon name: plus */}
-                <svg
-                  className="-ml-1 mr-2 h-5 w-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <span>New Job</span>
+                <FontAwesomeIcon icon={faPlus} className="-ml-1 mr-2 h-3 w-3" />
+                <span>Register a Club</span>
               </button>
             </div>
           </div>
@@ -131,12 +108,12 @@ export default function Navbar() {
 
     Menu open: "block", Menu closed: "hidden"
   */}
-      <div className="hidden md:hidden">
+      <div className="block lg:hidden">
         <div className="pt-2 pb-3 space-y-1">
           {/* Current: "bg-indigo-50 border-yei-primary-main text-yei-primary-darker", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
           <a
             href="#"
-            className="bg-indigo-50 border-yei-primary-main text-yei-primary-darker block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+            className="bg-indigo-50 border-yei-primary-main text-green-900 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
           >
             Dashboard
           </a>
@@ -158,64 +135,6 @@ export default function Navbar() {
           >
             Calendar
           </a>
-        </div>
-        <div className="pt-4 pb-3 border-t border-gray-200">
-          <div className="flex items-center px-4 sm:px-6">
-            <div className="flex-shrink-0">
-              <img
-                className="h-10 w-10 rounded-full"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-              />
-            </div>
-            <div className="ml-3">
-              <div className="text-base font-medium text-gray-800">
-                Tom Cook
-              </div>
-              <div className="text-sm font-medium text-gray-500">
-                tom@example.com
-              </div>
-            </div>
-            <button className="ml-auto flex-shrink-0 bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yei-primary-main">
-              <span className="sr-only">View notifications</span>
-              {/* Heroicon name: bell */}
-              <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-            </button>
-          </div>
-          <div className="mt-3 space-y-1">
-            <a
-              href="#"
-              className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
-            >
-              Your Profile
-            </a>
-            <a
-              href="#"
-              className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
-            >
-              Settings
-            </a>
-            <a
-              href="#"
-              className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
-            >
-              Sign out
-            </a>
-          </div>
         </div>
       </div>
     </nav>
