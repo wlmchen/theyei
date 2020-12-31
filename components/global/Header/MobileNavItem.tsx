2d3ae8acaa75fe5f0803f28c2dc62750b6a15378
MasonWang025 <mason.wang0025@gmail.com>
2020-12-30 18:57:07 -0800

    fix scroll bar

--- components/global/Header/MobileNavItem.tsx
+++ components/global/Header/MobileNavItem.tsx
@@ -67,7 +67,7 @@ function NestedItem({ href, name, active }) {
   const activeStyles = "bg-indigo-50 border-yei-primary-main text-green-900";
   const defaultStyles =
     "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700";
-  const navItemStyle = `trans-300 block pl-8 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6 ${
+  const navItemStyle = `trans-300 block pl-8 pr-4 py-2 border-l-4 text-base font-medium sm:pr-6 ${
     active ? activeStyles : defaultStyles
   }`;
 
