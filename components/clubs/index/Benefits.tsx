6c602638200782c5f53539bbab5c9d308dd5f49b
MasonWang025 <mason.wang0025@gmail.com>
2021-01-02 09:39:20 -0800

    refactor

--- components/clubs/index/Benefits.tsx
+++ components/clubs/index/Benefits.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBook,
-  faCalendar,
-  faCalendarWeek,
-  faGlobe,
-  faUsers,
-} from "@fortawesome/free-solid-svg-icons";
+
 import Link from "next/link";
+import benefits from "../../../data/content/clubBenefits";
 
 export default function Benefits() {
   return (
@@ -27,40 +22,7 @@ export default function Benefits() {
         </div>
         <div className="mt-10">
           <dl className=" space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-            {[
-              {
-                title: "Resources",
-                content:
-                  "Whether you're a new club just starting out or a pre-existing club wanting to join, Youth Economics Initiative will provide you with invaluable resources including our comprehensive AP/IB Economics club curriculum and our FLIP Personal Finance club workshop guide.",
-                href: "/resources",
-                ctaText: "Get Access Now",
-                icon: faBook,
-              },
-              {
-                title: "Network",
-                content:
-                  "Take advantage of the Youth Economics Initiative's network of leaders, creators, and entrepreneurs. You'll form connections with FBLA champions, business leaders, and entrepreneurs while sharpening your leadership and teamwork skills.",
-                href: "/events/econtalks",
-                ctaText: "Our Professional Network",
-                icon: faGlobe,
-              },
-              {
-                title: "Community",
-                content:
-                  "Join a community of problem-solvers, innovators, and leaders. By joining YEI, you join a movement towards empowering students with economic literacy and uniting economics clubs throughout the world.",
-                href: "https://discord.gg/RC588cv",
-                ctaText: "Join our Discord",
-                icon: faUsers,
-              },
-              {
-                title: "Events",
-                content:
-                  "Beyond club resources and mentorship, the YEI offers a variety of exclusive events, including EconBowl, EconOlympiad, EconTalks, leadership conferences, and more.",
-                href: "/events",
-                ctaText: "Join Our Events",
-                icon: faCalendarWeek,
-              },
-            ].map((reason) => (
+            {benefits.map((reason) => (
               <div key={reason.href} className="flex">
                 <div className="flex-shrink-0">
                   <div className="flex items-center justify-center h-12 w-12 rounded-md bg-yei-primary-main text-white">
---
+++ data/content/clubBenefits.ts
@@ -0,0 +1,44 @@
+import {
+  faBook,
+  faCalendar,
+  faCalendarWeek,
+  faGlobe,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons";
+
+const benefits = [
+  {
+    title: "Resources",
+    content:
+      "Whether you're a new club just starting out or a pre-existing club wanting to join, Youth Economics Initiative will provide you with invaluable resources including our comprehensive AP/IB Economics club curriculum and our FLIP Personal Finance club workshop guide.",
+    href: "/resources",
+    ctaText: "Get Access Now",
+    icon: faBook,
+  },
+  {
+    title: "Network",
+    content:
+      "Take advantage of the Youth Economics Initiative's network of leaders, creators, and entrepreneurs. You'll form connections with FBLA champions, business leaders, and entrepreneurs while sharpening your leadership and teamwork skills.",
+    href: "/events/econtalks",
+    ctaText: "Our Professional Network",
+    icon: faGlobe,
+  },
+  {
+    title: "Community",
+    content:
+      "Join a community of problem-solvers, innovators, and leaders. By joining YEI, you join a movement towards empowering students with economic literacy and uniting economics clubs throughout the world.",
+    href: "https://discord.gg/RC588cv",
+    ctaText: "Join our Discord",
+    icon: faUsers,
+  },
+  {
+    title: "Events",
+    content:
+      "Beyond club resources and mentorship, the YEI offers a variety of exclusive events, including EconBowl, EconOlympiad, EconTalks, leadership conferences, and more.",
+    href: "/events",
+    ctaText: "Join Our Events",
+    icon: faCalendarWeek,
+  },
+];
+
+export default benefits;
