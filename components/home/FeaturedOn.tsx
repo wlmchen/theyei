db30a4a0ddff688dba2dcee8a1a5b70134525ec3
MasonWang025 <mason.wang0025@gmail.com>
2021-01-02 08:51:26 -0800

    fix

--- components/home/FeaturedOn.tsx
+++ components/home/FeaturedOn.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import featuredon from "../../data/companies/featuredOn";
 
 export default function FeaturedOn() {
   return (
@@ -11,7 +10,36 @@ export default function FeaturedOn() {
           </h2>
           <div className="col-span-2 flow-root self-center mt-8 lg:mt-0">
             <div className="-mt-4 -ml-8 flex md:flex-nowrap flex-wrap justify-between lg:-ml-4">
-              {featuredon.map((link) => (
+              {[
+                {
+                  href:
+                    "https://markets.businessinsider.com/news/stocks/youth-run-nonprofit-transforming-economics-and-financial-literacy-education-1029503063#",
+                  imgURL: "img/logos/other/featured/business-insider.png",
+                  brightness: 1.6,
+                  alt: "business insider",
+                },
+                {
+                  href:
+                    "https://finance.yahoo.com/news/youth-run-nonprofit-transforming-economics-203600069.html",
+                  imgURL: "img/logos/other/featured/yahoo-finance.png",
+                  brightness: 2.2,
+                  alt: "yahoo finance",
+                },
+                {
+                  href:
+                    "https://www.marketwatch.com/press-release/youth-run-nonprofit-transforming-economics-and-financial-literacy-education-2020-08-13",
+                  imgURL: "img/logos/other/featured/market-watch.png",
+                  brightness: 1.1,
+                  alt: "market watch",
+                },
+                {
+                  href:
+                    "https://news.yahoo.com/news/youth-run-nonprofit-transforming-economics-203600069.html",
+                  imgURL: "img/logos/other/featured/yahoo-news.png",
+                  brightness: 2.2,
+                  alt: "yahoo news",
+                },
+              ].map((link) => (
                 <div
                   key={link.href}
                   className="mt-4 ml-8 flex flex-grow flex-shrink-0 justify-center lg:flex-grow-0 lg:ml-4"
