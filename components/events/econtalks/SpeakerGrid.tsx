75bf8f5d731ef194cfe17c4a4821768b30fd0fda
MasonWang025 <mason.wang0025@gmail.com>
2021-01-02 20:59:08 -0800

    update spacing

--- components/events/econtalks/SpeakerGrid.tsx
+++ components/events/econtalks/SpeakerGrid.tsx
@@ -8,7 +8,7 @@ export default function SpeakerGrid() {
       <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-14 mb-2">
         <div className="space-y-12">
           <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
-            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
+            <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl -mb-4">
               Previous Speakers
             </h2>
             {/* <p className="text-xl text-gray-500">
