8c8e92c4070a9b993d236dfed16abe7fc1406736
MasonWang025 <mason.wang0025@gmail.com>
2021-02-09 21:08:26 -0800

    mass update

--- next.config.js
+++ next.config.js
@@ -4,4 +4,18 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:slug",
+        destination: "/events/:slug",
+        permanent: true,
+      },
+      {
+        source: "/_error",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
