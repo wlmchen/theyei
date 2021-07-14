a09edef22b38d5d410f336d412c21ed1d3ccabbf
braydentw <reshiramfire130@gmail.com>
2021-07-13 22:25:35 -0500

    localstorage "hideHiringModal" onload in /apply

--- pages/people/apply.tsx
+++ pages/people/apply.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Hero from "../../components/people/apply/Hero";
 import Page from "../../components/utility/Page";
 
 export default function apply() {
+  useEffect(() => {
+    localStorage.setItem('hideHiringModal', 'yes')
+  }, [])
   return (
     <Page
       title="Apply"
