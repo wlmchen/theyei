9dff6d76bad26635bb9f4b8be2e565895e69dc6b
Brayden W <brayden45.dev@gmail.com>
2021-08-07 19:38:56 -0400

    Installed Google Tag Manager

--- components/utility/Page.tsx
+++ components/utility/Page.tsx
@@ -62,6 +62,14 @@ export default function Page({ fullTitle, title, desc, children }: PageProps) {
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
         />
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KC3CN7V');</script>
+<!-- End Google Tag Manager -->
+
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -78,6 +86,12 @@ export default function Page({ fullTitle, title, desc, children }: PageProps) {
       <Navbar />
       {children}
       <Footer />
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KC3CN7V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
     </>
   )
 }
