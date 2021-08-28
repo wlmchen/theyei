5b55e4bfd1d8165e89814db33b35b97ced1454be
braydentw <reshiramfire130@gmail.com>
2021-08-28 13:26:00 -0500

    Converted gtag to jsx syntax

--- components/utility/Page.tsx
+++ components/utility/Page.tsx
@@ -61,13 +61,15 @@ export default function Page({ fullTitle, title, desc, children }: PageProps) {
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
         />
-
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KC3CN7V');</script>
-
+})(window,document,'script','dataLayer','GTM-KC3CN7V');`,
+          }}
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -81,13 +83,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
       </Head>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KC3CN7V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+      ></noscript>
       <Navbar />
       {children}
       <Footer />
-
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KC3CN7V"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
     </>
   )
 }
