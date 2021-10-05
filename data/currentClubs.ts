081f7e740c90b141888a32a08a978fd8f08cc58f
ʙ ʀ ᴀ ʏ ᴅ ᴇ ɴ <brayden45.dev@gmail.com>
2021-10-05 16:54:18 -0500

    Update currentClubs.ts

--- data/currentClubs.ts
+++ data/currentClubs.ts
@@ -359,7 +359,7 @@ const clubs = [
   {
     name: 'Al Ain Juniors School',
     email: 'noufalthanduthiyil@gmail.com',
-    region: 'India',
+    region: 'Uniter Arab Emirates',
   },
   {
     name: 'The Henrietta Barnett School',
