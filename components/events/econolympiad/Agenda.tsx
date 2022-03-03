b37faaa47097c22858afa53a16adc372002d71a2
Brayden <brayden45.dev@gmail.com>
2022-03-02 18:25:14 -0600

    Update Agenda.tsx

--- components/events/econolympiad/Agenda.tsx
+++ components/events/econolympiad/Agenda.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const openingSchedule = [
-  { time: '9:30 am', title: 'Opening Ceremony' },
-  { time: '10:00 am - 11:00 am', title: 'Speaker Presentation' },
-  { time: '11:15 am', title: 'Raffle' },
-  { time: '11:30 am', title: 'Prompts Announced' },
+  { time: '9:00am', title: 'Opening Ceremony' },
+  { time: '9:30am - 10:30 am', title: 'Speaker Presentation' },
+  { time: '11:00 am', title: 'Raffle' },
+  { time: '11:15 am', title: 'Prompts Announced' },
 ]
 
 const ceremonySchedule = [
-  { time: '9:00 am - 10:00 am', title: 'Debate Round' },
+  { time: '9:00 am - 12:00 am', title: 'Debate Round' },
   { time: '4:00 pm - 5:00 pm', title: 'Awards Ceremony' },
 ]
 
@@ -24,13 +24,13 @@ export default function Agenda() {
             <div className="md:w-1/2">
               <ScheduleTable
                 schedule={openingSchedule}
-                title="Opening Ceremony Schedule (3/19)"
+                title="Opening Ceremony Schedule (3/19/22)"
               />
             </div>
             <div className="md:w-1/2">
               <ScheduleTable
                 schedule={ceremonySchedule}
-                title={`Debate Round & Awards Ceremony (4/23)`}
+                title="Debate Round & Awards Ceremony (4/23/22)"
               />
             </div>
           </div>
