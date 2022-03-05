ffbc8e30d64b04a96fd14c6c916f5f65d9f6806d
Brayden <brayden45.dev@gmail.com>
2022-03-05 12:46:31 -0600

    Update Speaker.tsx

--- components/events/econolympiad/Speaker.tsx
+++ components/events/econolympiad/Speaker.tsx
@@ -5,12 +5,7 @@ export default function Speaker() {
     <div className="bg-white pt-16 lg:py-24">
       <div className="pb-16 bg-yei-secondary-brighter lg:pb-0 lg:z-10 lg:relative">
         <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8">
-          <div className="relative lg:-my-8">
-            <div
-              aria-hidden="true"
-              className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden"
-            />
-            <div className="hidden lg:block mx-auto max-w-md px-4 sm:max-w-4xl sm:px-6 lg:p-0 lg:h-full">
+            <div className="hidden lg:block mx-auto max-w-md py-4 px-4 sm:max-w-4xl sm:px-6 lg:p-0 lg:h-full">
               <div className="rounded-xl shadow-xl overflow-hidden lg:h-full">
                 <img
                   className="object-cover lg:h-full lg:w-full"
@@ -19,7 +14,6 @@ export default function Speaker() {
                 />
               </div>
             </div>
-          </div>
           <div className="mt-2 lg:m-0 lg:col-span-2 lg:pl-8">
             <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 lg:max-w-none">
               <div>
