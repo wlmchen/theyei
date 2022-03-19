f31508039d97ca30e072d1a3a93244e598e6a48d
Brayden <brayden45.dev@gmail.com>
2022-03-18 20:05:55 -0500

    Update Hero.tsx

--- components/events/econolympiad/Hero.tsx
+++ components/events/econolympiad/Hero.tsx
@@ -75,8 +75,6 @@ export default function Hero() {
                   </p>
                   <p className="mt-4">
                     Interested? EconOlympiad is open to ALL middle and high school students!
-                    Register below by{' '}
-                    <strong>3/17/22</strong>.
                   </p>
                   <p className="mt-4">
                     The final deadline to enter the competition is{' '}
