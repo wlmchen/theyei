f44710fddf2377cd862499f21260d105ad7eb56e
braydentw <reshiramfire130@gmail.com>
2022-03-29 20:26:01 -0500

    Update links

--- components/events/econolympiad/Info.tsx
+++ components/events/econolympiad/Info.tsx
@@ -1,3 +1,5 @@
+import { faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 export default function Info() {
@@ -117,44 +119,57 @@ export default function Info() {
             </span>
           </h1>
           <p className="mt-8 text-xl text-gray-500 leading-8">
-            EconOlympiad is a two-part competition focused on the application, not merely the 
-            knowledge, of economics. The first part is a presentation round in which you and 
-            your team will have three weeks to develop and submit a video that responds to one 
-            of the two provided prompts. The second part is a debate round. After completing 
-            the presentation, top teams will advance to a debate round. A prompt will be released, 
-            and teams will have one week to research and prepare. will compose your team’s 
-            final score.
+            EconOlympiad is a two-part competition focused on the application,
+            not merely the knowledge, of economics. The first part is a
+            presentation round in which you and your team will have three weeks
+            to develop and submit a video that responds to one of the two
+            provided prompts. The second part is a debate round. After
+            completing the presentation, top teams will advance to a debate
+            round. A prompt will be released, and teams will have one week to
+            research and prepare. will compose your team’s final score.
           </p>
           <p className="mt-6 text-xl text-gray-500 leading-8">
-            Professors from the country’s top economics programs will serve as judges for both the 
-            presentation and debate rounds.
+            Professors from the country’s top economics programs will serve as
+            judges for both the presentation and debate rounds.
           </p>
+
+          <a
+            href="https://www.youtube.com/watch?v=XxS0PSG9aro"
+            target="_blank"
+            rel="noreferrer"
+            className="inline-flex items-center mt-3 px-6 py-2 rounded-full text-base font-medium bg-green-100 hover:text-green-900 trans-300 text-green-700"
+          >
+            <FontAwesomeIcon icon={faYoutube} className="h-6 w-6 mr-3" />
+            2022 EconOlympiad Prompt & Guidelines
+          </a>
         </div>
         <div className="mt-8 prose prose-green prose-lg text-gray-500 mx-auto">
           <h2>Presentation Round</h2>
           <p>
-            Competitors will have up to one week to create their presentation up to 10 minutes in 
-            duration. There will be 2 prompts to choose from: one that focuses on Microeconomics 
-            and the other that concentrates on Macroeconomics concepts. Your group will have a 
-            chance to research the prompts, take a position, provide facts to support your argument, 
-            and use your reasoning to tie it all together. 
+            Competitors will have up to one week to create their presentation up
+            to 10 minutes in duration. There will be 2 prompts to choose from:
+            one that focuses on Microeconomics and the other that concentrates
+            on Macroeconomics concepts. Your group will have a chance to
+            research the prompts, take a position, provide facts to support your
+            argument, and use your reasoning to tie it all together.
           </p>
           <h2>Debate Round</h2>
           <p>
-            Top teams will participate in a debate with a prompt different from previous choices, 
-            determining the final EconOlympiad winners.
+            Top teams will participate in a debate with a prompt different from
+            previous choices, determining the final EconOlympiad winners.
           </p>
 
           <h2>Team Matching: A New Way To Find Members</h2>
           <p>
-            Are you looking to join a team or would like some extra members to join yours?
-            Join our{' '}
+            Are you looking to join a team or would like some extra members to
+            join yours? Join our{' '}
             <a href="https://discord.gg/RC588cv" target="_blank">
               Discord server
             </a>{' '}
-            and react to the “finding teams” message in the “EconOlympiad” channel! Doing 
-            this will allow you to join a channel specifically for team finding, where you 
-            will be able to meet others who are looking for teammates as well.{' '}
+            and react to the “finding teams” message in the “EconOlympiad”
+            channel! Doing this will allow you to join a channel specifically
+            for team finding, where you will be able to meet others who are
+            looking for teammates as well.{' '}
           </p>
           <p>
             If you are not able to give yourself the role via the above process
