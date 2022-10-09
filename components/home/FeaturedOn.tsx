efb0328e5b8651d64a189eb1c6dadfcec537a783
braydentw <reshiramfire130@gmail.com>
2022-10-08 19:03:15 -0500

    Fix max width on featured logos

--- components/home/FeaturedOn.tsx
+++ components/home/FeaturedOn.tsx
@@ -8,7 +8,7 @@ export default function FeaturedOn() {
           <h2 className="w-full text-xl mb-5 uppercase text-gray-400 font-black text-center max-w-xs border-b-2 border-gray-300 pb-5">
             Featured On
           </h2>
-            <div className="w-full max-w-sm md:max-w-2xl m-auto flex flex-row flex-wrap justify-center items-center text-center">
+            <div className="w-full max-w-sm md:max-w-3xl m-auto flex flex-row flex-wrap justify-center items-center text-center">
               {featured.map((link) => (
                   <img
                     className="h-12 lg:h-14 mx-4 my-2"
