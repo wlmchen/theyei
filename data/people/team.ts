af54984a189d79314bea0060a40608ca02b53aec
wlmchen <65417452+wlmchen@users.noreply.github.com>
2024-09-16 00:36:33 -0700

    Update team.ts

--- data/people/team.ts
+++ data/people/team.ts
@@ -159,7 +159,8 @@ const team: Member[] = [
   },
   {
     name: 'Peter Yang',
-    positions: ['Sponsorship and Grant Coordinator']
+    positions: ['Sponsorship and Grant Coordinator'],
+    linkedin: 'peter-yang26'
   },
   {
     name: 'Pratyaksha Sharma',
